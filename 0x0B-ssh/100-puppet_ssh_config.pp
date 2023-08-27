23d5403154cfdb5cef4084b8af626d453ebb68c4
ShehabNegm <shebonegm@gmail.com>
2023-08-27 15:09:23 -0700

    fix errors

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,12 +3,12 @@ include stdlib
 
 file_line { 'private key adding':
   path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/school',
+  line    => '        IdentityFile ~/.ssh/school',
   replace => true,
 }
 
 file_line { 'private key adding':
   path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no',
+  line    => '        PasswordAuthentication no',
   replace => true
 }

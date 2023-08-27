397dcde43fa8cfa95788ba326ee0257207595fef
ShehabNegm <shebonegm@gmail.com>
2023-08-27 15:04:36 -0700

    fix errors

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,14 @@
 # pupet script to set client side SSH config file
 include stdlib
+
 file_line { 'private key adding':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/school'
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  replace => true,
 }
 
 file_line { 'private key adding':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no'
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  replace => true
 }
